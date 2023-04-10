08afd569414a6a78937184f1b7414f538265e312
ytMoon <moonyt@kr.doubledown.com>
2023-04-10 18:18:03 +0900

    format save

--- src/BackGroundScript.ts
+++ src/BackGroundScript.ts
@@ -1,43 +1,45 @@
-let interval = 1000;
-const func = function(interval: number){
+chrome.tabs.executeScript({ code: 'document.querySelector("body")' }, () => {
+  let interval = 1000;
+  const func = function (interval: number) {
     clearTimeout(timeoutID);
-    interval =  Math.random() * 500;
+    interval = Math.random() * 500;
     const isBodyLoaded = !!document.body;
-    if(isBodyLoaded){
-      
+    if (isBodyLoaded) {
+
     }
-    timeoutID = setTimeout(func,interval);
-}
+    timeoutID = setTimeout(func, interval);
+  }
 
-let timeoutID = setTimeout(func,interval);
+  let timeoutID = setTimeout(func, interval);
 
-//   var func = function(hollIndex,itemIndex,time){
-//     hollIndex--;
-//     itemIndex--;
-//     var hollCont_contain = document.getElementsByClassName("hollcont_contain")
-//     var row_list = hollCont_contain[0].children[0].children[hollIndex].children[0];
-    
-//     var holl_tit = document.getElementsByClassName("holltit_w");
-//     var hollName = holl_tit[0].children[0].children[hollIndex].firstChild;
-    
-//     var engChar = "";
-//     switch(hollIndex){
-//         case 0:
-//             engChar = 'A';
-//             break;
-//         case 1:
-//             engChar = 'B';
-//             break;
-//         case 2:
-//             engChar = 'C';
-//             break;
-//         case 3:
-//             engChar = 'D';
-//             break;
-//     }
-    
-//     fnChoiceCourseTime(row_list.children[itemIndex].firstChild, engChar, hollName,time+"");
-//     window.scrollTo(0,2000);
-//     document.getElementById("golfAgreeN").checked = false;
-//     document.getElementById("golfAgreeY").checked = true;
-// }
+  //   var func = function(hollIndex,itemIndex,time){
+  //     hollIndex--;
+  //     itemIndex--;
+  //     var hollCont_contain = document.getElementsByClassName("hollcont_contain")
+  //     var row_list = hollCont_contain[0].children[0].children[hollIndex].children[0];
+
+  //     var holl_tit = document.getElementsByClassName("holltit_w");
+  //     var hollName = holl_tit[0].children[0].children[hollIndex].firstChild;
+
+  //     var engChar = "";
+  //     switch(hollIndex){
+  //         case 0:
+  //             engChar = 'A';
+  //             break;
+  //         case 1:
+  //             engChar = 'B';
+  //             break;
+  //         case 2:
+  //             engChar = 'C';
+  //             break;
+  //         case 3:
+  //             engChar = 'D';
+  //             break;
+  //     }
+
+  //     fnChoiceCourseTime(row_list.children[itemIndex].firstChild, engChar, hollName,time+"");
+  //     window.scrollTo(0,2000);
+  //     document.getElementById("golfAgreeN").checked = false;
+  //     document.getElementById("golfAgreeY").checked = true;
+  // }
+});
